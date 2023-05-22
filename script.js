9ceeb252e3d4620a25d0706321bc5fb7a09bdf3b
Kartikey Singh <92157475+Kartikey06dk@users.noreply.github.com>
2023-05-22 11:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
-
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
 //your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
+
+class OutOfRangeError extends Error {
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should only consist of integers and +-/* characters and not <arg>";
+  }
+}
+
+class InvalidExprError extends Error {
+  constructor() {
+    super();
+    this.name = this.constructor.name;
+    this.message = "Expression should not have an invalid combination of expression";
+  }
+}
+
+function evalString(expression) {
+  if (expression.match(/[-+*/]{2}/g)) {
+    throw new InvalidExprError();
+  }
+
+  if (expression.match(/^[+*/]/)) {
+    throw new SyntaxError("Expression should not start with an invalid operator");
+  }
+
+  if (expression.match(/[-+*/]$/)) {
+    throw new SyntaxError("Expression should not end with an invalid operator");
+  }
+
+  const numbers = expression.split(/[-+*/]/);
+  for (let i = 0; i < numbers.length; i++) {
+    if (!/^[-+]?\d+$/.test(numbers[i].trim())) {
+      throw new OutOfRangeError();
+    }
+  }
+
+  const operators = expression.split(/\d+/).filter(Boolean);
+  for (let i = 0; i < operators.length; i++) {
+    if (!/[-+*/]/.test(operators[i].trim())) {
+      throw new OutOfRangeError();
+    }
+  }
+
+  // Evaluate the expression
+  const result = eval(expression);
+  return result;
+}
+
+// Testing the evalString function
+/*try {
+  const expression = "10 + 5 - 2 * 3 / 4";
+  const result = evalString(expression);
+  console.log("Result:", result);
+} catch (error) {
+  console.error("Error:", error.message);
+}
+*/
